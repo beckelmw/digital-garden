3396d34b752aa0fae76547897d94ef7ce603f1db
Bill Beckelman <bill@beckelman.net>
2022-03-08 12:40:12 -0700

    Added build for meta

--- build/meta.js
+++ build/meta.js
@@ -5,12 +5,9 @@ import getMarkdown from "./lib/get-markdown.js";
 const files = await getFiles("**/*.md");
 const mdFiles = await getMarkdown(files);
 
-const json = mdFiles.map(({ markdown, meta }) => {
-  return {
-    title: meta.title,
-    text: markdown,
-    url: meta.url,
-  };
-});
-
-writeFile("./search.json", JSON.stringify(json));
+const meta = Object.fromEntries(
+  mdFiles.map(({ meta }) => {
+    return [meta.url, meta];
+  })
+);
+writeFile("./meta.json", JSON.stringify(meta, null, 2));
